5302920d3f4f95387df922775e184e771b1b1fef
Mario Castillo <josemariocastillo06@gmail.com>
2026-05-25 09:57:37 -0600

    FEAT: crear clase suma que permite la suma de dos números

--- src/Suma.java
+++ src/Suma.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Suma {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese el primer número: ");
+        int a = sc.nextInt();
+        System.out.print("Ingrese el segundo número: ");
+        int b = sc.nextInt();
+        int suma = a + b;
+        System.out.println("La suma es: " + suma);
+    }
 }
+
